15a1f20589d15565257c4ed55ab75938b6f3a89d
Dmitry <chernuhadmitry@gmail.com>
2021-12-01 12:35:44 +0300

    Finish

--- main.py
+++ main.py
@@ -1,53 +1,89 @@
 import tkinter as tk
-
-# Создаем функцию добавления цифр в окно ввода
+from tkinter import messagebox
 
 
 def add_digit(digit):
+    """Функция добавления цифр в окно ввода"""
     value = calc.get()
-    if value[0] == '0':
+    if value[0] == '0' and len(value) == 1:
         value = value[1:]
     calc.delete(0, tk.END)
     calc.insert(0, value+digit)
 
-# Создаем функцию добавления знаки операций в окно ввода
-
 
 def add_operations(operations):
+    """Функция добавления знаки операций в окно ввода"""
     value = calc.get()
     if value[-1] in '+-*/':
         value = value[:-1]
+    elif '+' in value or '-' in value or '*' in value or '/' or value:
+        calculate()
+        value = calc.get()
     calc.delete(0, tk.END)
     calc.insert(0, value+operations)
 
 
-# Создаем функцию создания кнопок цифр
-
 def make_digit_buttons(digit):
+    """Функция создает кнопки с цифрами"""
     return tk.Button(text=digit, bd=5, font=('Arial', 15), command=lambda: add_digit(digit))
 
 
-# Создаем функцию создания знаков операций
-
-
 def make_operations_buttons(operations):
+    """Функция создания знаков операций"""
     return tk.Button(text=operations, bd=5, font=('Arial', 15), fg='red', command=lambda: add_operations(operations))
 
 
 def make_calc_buttons(operations):
-    return tk.Button(text=operations, bd=5, font=('Arial', 15), fg='red', command=lambda: add_digit(operations))
+    """Функция создает кнопку равно"""
+    return tk.Button(text=operations, bd=5, font=('Arial', 15), fg='red', command=calculate)
 
 
+def make_clear_buttons(operations):
+    """Функция создает кнопку очистить"""
+    return tk.Button(text=operations, bd=5, font=('Arial', 15), fg='red', command=clear)
 
-# Создаем окно приложения
+
+def clear():
+    calc.delete(0, tk.END)
+    calc.insert(0, 0)
+
+
+def calculate():
+    """Функция для подсчета значений"""
+    value = calc.get()
+    if value[-1] in '+-*/':
+        operation = value[-1]
+        value = value[:-1] + operation + value[:-1]
+    calc.delete(0, tk.END)
+    try:
+        calc.insert(0, eval(value))
+    except (NameError, SyntaxError):
+        messagebox.showinfo('Внимание', 'Введите цифры' )
+        calc.insert(0, 0)
+    except ZeroDivisionError:
+        messagebox.showinfo('Внимание', 'На ноль делить нельзя')
+        calc.insert(0, 0)
+
+
+def press_key(event):
+    """Функция обработки событий клавиатуры"""
+    print(event.char)
+    if event.char.isdigit():
+        add_digit(event.char)
+    elif event.char in '+-*/':
+        add_operations(event.char)
+    elif event.char == '\r':
+        calculate()
 
 
 win = tk.Tk()
-win.geometry(f'240x270+600+200')
+win.geometry(f'240x270+850+400')
 win['bg'] = '#909090'
 win.title('Калькулятор')
 faw_icon = tk.PhotoImage(file='fawicon.png')
 win.iconphoto(False, faw_icon)
+
+win.bind('<Key>', press_key)
 
 # Создаем поле для ввода цифр
 
@@ -55,7 +91,7 @@ calc = tk.Entry(win, justify=tk.RIGHT, font=('Arial', 15), width=15)
 calc.insert(0, '0')
 calc.grid(row=0, column=0, columnspan=4, stick='we', padx=5)
 
-# Создаем кнопки для цифр
+# Создаем кнопки
 
 make_digit_buttons('1').grid(row=1, column=0, stick='wens', padx=5, pady=5)
 make_digit_buttons('2').grid(row=1, column=1, stick='wens', padx=5, pady=5)
@@ -74,7 +110,7 @@ make_operations_buttons('/').grid(row=3, column=3, stick='wens', padx=5, pady=5)
 make_operations_buttons('*').grid(row=4, column=3, stick='wens', padx=5, pady=5)
 
 make_calc_buttons('=').grid(row=4, column=2, stick='wens', padx=5, pady=5)
-
+make_clear_buttons('C').grid(row=4, column=1, stick='wens', padx=5, pady=5)
 
 win.grid_columnconfigure(0, minsize=60)
 win.grid_columnconfigure(1, minsize=60)
@@ -87,4 +123,6 @@ win.grid_rowconfigure(3, minsize=60)
 win.grid_rowconfigure(4, minsize=60)
 
 win.mainloop()
+
+
 
